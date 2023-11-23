3d55436abe3e869ae071883bdcebc670c42d0b26
Jinyuan Zhang <jinyuanzhang1992@hotmail.com>
2023-11-23 16:53:35 +0930

    new deploy json server and web server, create index.js to prepare the express framework, create .gitignore file

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+//require some modules
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const moment = require("moment");
+const bodyParser = require("body-parser");
+
+// create an web server
+const app = express();
+
+// require global middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//require router modules
+
+// use router modules
+
+//run the server
+app.listen(50009, () => {
+  console.log("Server is running on http://localhost:50009");
+});
