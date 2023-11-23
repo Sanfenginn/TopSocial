ca9c0296e2737e533df12ef310e93c878090b1c4
Jinyuan Zhang <jinyuanzhang1992@hotmail.com>
2023-11-24 00:30:42 +0930

    test

--- backend/routers/profile.js
+++ backend/routers/profile.js
@@ -2,20 +2,38 @@ const express = require("express");
 const axios = require("axios");
 const profileRouter = express.Router();
 
+//.then .catch 方法
+// profileRouter.get("/profile", (req, res, next) => {
+//   const url = "http://localhost:50010/profile";
+//   axios
+//     .get(url)
+//     .then((axiosResponse) => {
+//       //这里promise的结果不能用res，因为res是express的，axiosResponse是axios的，如果用res会跟express的冲突
+//       const responseData = axiosResponse.data;
+//       console.log("response: ", responseData);
+//       res.status(200).json({
+//         status: 200,
+//         msg: "get profile successfully",
+//         data: responseData,
+//       });
+//     })
+//     .catch((err) => {
+//       console.error("error: ", err);
+//       next(err);
+//     });
+// });
+
+// async/await方法
 profileRouter.get("/profile", async (req, res, next) => {
   try {
     const url = "http://localhost:50010/profile";
-    const response = await axios.get(url);
-    //get拿到数据，
-    console.log("response: ", response.data);
-    //测试能从json server拿到数据
-    res.status(200).send({
+    const responseDate = await axios.get(url);
+    console.log("response: ", responseDate.data);
+    res.status(200).json({
       status: 200,
       msg: "get profile successfully",
-      data: response.data,
+      data: responseDate.data,
     });
-    //send给前端
-    // throw new Error("Server Error"); //Server Error 就是message
   } catch (error) {
     console.error("error: ", error);
     next(error);
