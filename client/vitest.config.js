58bb457b7e080a77669dd952e89ca3c00b6f82a8
Jinyuan Zhang <jinyuanzhang1992@hotmail.com>
2023-12-01 02:47:46 +0930

    test the unit testing for client

--- client/vitest.config.js
+++ client/vitest.config.js
@@ -10,11 +10,11 @@ export default defineConfig({
   test: {
     env: {
       MODE: "development", // 设置默认模式为 'test'
-      // VITE_DB_PATH: "localhost:51002",
+      VITE_DB_PATH: "project1.sanfenginn.com/api2/", //"localhost:51002",
     },
     // 在此处添加你的测试配置
     globals: true,
-    environment: "jsdom", // 如果你的测试依赖于DOM或浏览器API
+    environment: "node", //"jsdom", // 如果你的测试依赖于DOM或浏览器API
     // include: ["src/**/*.test.js"], // 包含哪些文件，支持 glob 模式
     // exclude: ["src/**/*.ignore.js"], // 排除哪些文件，支持 glob 模式
   },
