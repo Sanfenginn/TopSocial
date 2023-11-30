135281d363da78a552f26013a08ee234389003d1
Jinyuan Zhang <jinyuanzhang1992@hotmail.com>
2023-12-01 06:16:21 +0930

    test the unit testing for client

--- client/src/js/utils.js
+++ client/src/js/utils.js
@@ -3,8 +3,8 @@ import path from "path-browserify";
 
 //GET FULLPATH
 const getPath = (...info) => {
-  // const dbPath = import.meta.env.VITE_DB_PATH;
-  const dbPath = "localhost:51002";
+  const dbPath = import.meta.env.VITE_DB_PATH;
+  // const dbPath = "localhost:51002";
 
   const fullPath = getHttpProtocol() + path.join(dbPath, ...info);
   return fullPath;
