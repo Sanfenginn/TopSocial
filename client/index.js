392cebcf4ac08c673cdf4fb63ed060c8fc9a33cd
Jinyuan Zhang <jinyuanzhang1992@hotmail.com>
2023-12-02 04:40:16 +0930

    add the sidebar at response design

--- client/index.js
+++ client/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import { profile } from "@/js/database.js";
 
 // IMPORT ALL THE JS FILES
+import "./src/css/navbar";
 import "./src/frames/main/main-middle/highline/highline";
 import "./src/frames/main/main-middle/postCards/postCards";
 import "./src/frames/main/main-right/message/message";
---
+++ client/src/css/navbar.js
@@ -0,0 +1,35 @@
+const navbar = document.querySelector(".bar");
+const barIcon = document.querySelector(".bar-icon");
+
+let navTimeout; // 用于存储定时器ID的变量
+
+function closeNav() {
+  navbar.classList.remove("open");
+  barIcon.classList.remove("open");
+  barIcon.classList.add("close");
+}
+
+barIcon.addEventListener("click", () => {
+  if (barIcon.classList.contains("close")) {
+    navbar.classList.add("open");
+    barIcon.classList.remove("close");
+    barIcon.classList.add("open");
+    clearTimeout(navTimeout); // 清除之前的定时器（如果有的话）
+    navTimeout = setTimeout(closeNav, 10000); // 设置10秒后关闭菜单的定时器
+  } else if (barIcon.classList.contains("open")) {
+    closeNav();
+    clearTimeout(navTimeout); // 清除定时器
+  }
+});
+
+window.addEventListener("scroll", () => {
+  closeNav();
+  clearTimeout(navTimeout); // 清除定时器
+});
+
+document.addEventListener("click", function (event) {
+  if (!navbar.contains(event.target) && !barIcon.contains(event.target)) {
+    closeNav();
+    clearTimeout(navTimeout); // 清除定时器
+  }
+});
